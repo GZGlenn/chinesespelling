eebc15bf074736e552c591467edecc2bf17841ae
glennbinhu(胡彬) <glennbinhu@tencent.com>
2018-08-18 17:48:03 +0800

    commit berfore merge

---
+++ src/main/java/com/pr/nlp/data/ChangeData2.java
@@ -0,0 +1,30 @@
+//package com.pr.nlp.data;
+//
+//public class ChangeData2 {
+//
+//    private int wordInd;
+//    private String changeStr;
+//
+//
+//    public ChangeData2(int wordInd, String changeStr) {
+//        this.wordInd = wordInd;
+//        this.changeStr = changeStr;
+//    }
+//
+//    public int getWordInd() {
+//        return wordInd;
+//    }
+//
+//    public void setWordInd(int wordInd) {
+//        this.wordInd = wordInd;
+//    }
+//
+//    public String getChangeStr() {
+//        return changeStr;
+//    }
+//
+//    public void setChangeStr(String changeStr) {
+//        this.changeStr = changeStr;
+//    }
+//
+//}
--- src/main/java/com/pr/nlp/data/SighanDataBean.java
+++ src/main/java/com/pr/nlp/data/SighanDataBean.java
@@ -1,18 +1,27 @@
 package com.pr.nlp.data;
 
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
+import javafx.util.Pair;
 import jdk.nashorn.internal.parser.JSONParser;
 import net.sf.json.JSON;
 import net.sf.json.JSONObject;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class SighanDataBean implements Serializable {
 
     private String idStr;
     private String content;
+    private String correctContent;
     private int location;
     private String errorStr;
     private String correctStr;
+
+    private ArrayList<ArrayList<ChangeData>> changeList;
 
     public SighanDataBean(String idStr, String content, int location, String errorStr, String correctStr) {
         this.idStr = idStr;
@@ -20,6 +29,8 @@ public class SighanDataBean implements Serializable {
         this.location = location;
         this.errorStr = errorStr;
         this.correctStr = correctStr;
+        this.changeList = new ArrayList<>();
+        this.correctContent = calCorrectContent();
     }
 
     public SighanDataBean(String idStr, String content) {
@@ -44,17 +55,19 @@ public class SighanDataBean implements Serializable {
         return correctStr;
     }
 
+    public String getCorrectContent() {
+        return correctContent;
+    }
+
+    public ArrayList<ArrayList<ChangeData>> getChangeList() {
+        return changeList;
+    }
+
     public void setIdStr(String idStr) {
         this.idStr = idStr;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setLocation(int location) {
-        this.location = location;
-    }
+    public void setLocation(int location) { this.location = location; }
 
     public void setErrorStr(String errorStr) {
         this.errorStr = errorStr;
@@ -62,6 +75,24 @@ public class SighanDataBean implements Serializable {
 
     public void setCorrectStr(String correctStr) {
         this.correctStr = correctStr;
+    }
+
+    public void setChangeList(ArrayList<ArrayList<ChangeData>> changeList) {
+        this.changeList = changeList;
+        for (int i = 0 ; i < this.changeList.size(); i++) {
+            this.changeList.get(i).sort(new Comparator<ChangeData>() {
+                @Override
+                public int compare(ChangeData o1, ChangeData o2) {
+                    return o1.getStartInd() - o2.getStartInd();
+                }
+            });
+        }
+    }
+
+    public String calCorrectContent() {
+        String prefix = content.substring(0, location);
+        String posfix = content.substring(location + errorStr.length(), content.length());
+        return prefix + this.correctStr + posfix;
     }
 
     public static SighanDataBean parseData(String line) {
